7488de5f28050c80d01b9dbeb4f40947ba1ce425
tn3rb <brent@eventespresso.com>
2024-05-10 12:49:08 -0700

    updates from cafe repo

--- eea-events-blog.php
+++ eea-events-blog.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
   Plugin Name:      Event Espresso - Front Page Events
   Plugin URI:       http://espressothemes.com/front-page-events/
@@ -19,18 +20,20 @@
  * @author     Darren Ethier
  */
 
-//add events into main blog query.
+// add events into main blog query.
 add_action('pre_get_posts', 'espresso_integrate_events_with_posts', 10);
-function espresso_integrate_events_with_posts($WP_Query){
+function espresso_integrate_events_with_posts($WP_Query)
+{
 
         /**
          * don't inject on post list posts.
          */
-        if (is_admin()) {
-           return;
-        }
+    if (is_admin()) {
+       return;
+    }
 
-        if ($WP_Query instanceof WP_Query
+    if (
+        $WP_Query instanceof WP_Query
             && $WP_Query->is_main_query()
             && ($WP_Query->is_feed
                 || $WP_Query->is_posts_page
@@ -38,20 +41,20 @@ function espresso_integrate_events_with_posts($WP_Query){
                     && ! $WP_Query->is_page
                 )
             )
-        ) {
-            //if post_types ARE present and 'post' is not in that array, then get out!
-            if (isset($WP_Query->query_vars['post_type']) && $post_types = (array)$WP_Query->query_vars['post_type']) {
-                if (! in_array('post', $post_types)) {
-                    return;
-                }
-            } else {
-                $post_types = array('post');
+    ) {
+        // if post_types ARE present and 'post' is not in that array, then get out!
+        if (isset($WP_Query->query_vars['post_type']) && $post_types = (array) $WP_Query->query_vars['post_type']) {
+            if (! in_array('post', $post_types)) {
+                return;
             }
-
-            if (! in_array('espresso_events', $post_types)) {
-                $post_types[] = 'espresso_events';
-                $WP_Query->set('post_type', $post_types);
-            }
-            return;
+        } else {
+            $post_types = array('post');
         }
+
+        if (! in_array('espresso_events', $post_types)) {
+            $post_types[] = 'espresso_events';
+            $WP_Query->set('post_type', $post_types);
+        }
+        return;
     }
+}
