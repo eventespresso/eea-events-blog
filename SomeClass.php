bc31f99f204aec84e7bbc2f94ab34761e173ca92
eeteamcodebase <team@eventespresso.com>
2025-04-01 17:30:49 -0700

    VERSION BUMP: 1.0.1

---
+++ SomeClass.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * SomeClass
+ *
+ * @package     Event Espresso
+ * @subpackage  ${NAMESPACE}
+ * @author      Brent Christensen
+ * @since       1.0.1
+ */
+class SomeClass
+{
+    /**
+     * @return void
+     * @since 1.0.1
+     */
+    public static function testingVersionUpdates()
+    {
+        echo 'Testing version updates...';
+    }
+}
--- eea-events-blog.php
+++ eea-events-blog.php
@@ -4,7 +4,7 @@
   Plugin Name:      Event Espresso - Front Page Events
   Plugin URI:       http://espressothemes.com/front-page-events/
   Description:      Injects events into your main blog query.
-  Version:          1.0.0
+  Version:          1.0.1
   Author:           Seth Shoultes
   Author URI:       http://eventespresso.com
   License:          GPL
@@ -25,22 +25,22 @@ add_action('pre_get_posts', 'espresso_integrate_events_with_posts', 10);
 function espresso_integrate_events_with_posts($WP_Query)
 {
 
-        /**
-         * don't inject on post list posts.
-         */
+    /**
+     * don't inject on post list posts.
+     */
     if (is_admin()) {
-       return;
+        return;
     }
 
     if (
         $WP_Query instanceof WP_Query
-            && $WP_Query->is_main_query()
-            && ($WP_Query->is_feed
-                || $WP_Query->is_posts_page
-                || ($WP_Query->is_home
-                    && ! $WP_Query->is_page
-                )
+        && $WP_Query->is_main_query()
+        && ($WP_Query->is_feed
+            || $WP_Query->is_posts_page
+            || ($WP_Query->is_home
+                && ! $WP_Query->is_page
             )
+        )
     ) {
         // if post_types ARE present and 'post' is not in that array, then get out!
         if (isset($WP_Query->query_vars['post_type']) && $post_types = (array) $WP_Query->query_vars['post_type']) {
